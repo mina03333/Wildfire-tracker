71951c0167b468f492398e31a1b5ac55b9e5eba7
Mina Yoon <56202580+mina03333@users.noreply.github.com>
2021-05-04 20:02:34 -0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import locationIcon from '@iconify/icons-mdi/fire-alert'
 const Header = () => {
     return (
         <header className="header">
-            <h1><Icon icon={locationIcon} /> Wildfire Tracker (Powered By NASA)</h1>
+            <h1><Icon icon={locationIcon} /> Wildfire Tracker</h1>
         </header>
     )
 }
